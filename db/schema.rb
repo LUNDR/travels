37bb2d4575b600d8a15da6fdbf6c01a8915e7e1c
LUNDR <lundrm@gmail.com>
2019-04-11 10:15:51 +0100

    added styling to posts, plus ability to add pictures via a url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_154048) do
+ActiveRecord::Schema.define(version: 2019_04_11_073656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_04_10_154048) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "banner_image_url"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
